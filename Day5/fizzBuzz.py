0ed620052bfb1eb89841b2585d48631f585c1456
Sriram48 <sriramkuma5121r@gmail.com>
2021-07-25 17:18:56 +0530

    Day 5 completed successfully

---
+++ Day5/fizzBuzz.py
@@ -0,0 +1,10 @@
+# Write your code below this row 👇
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FuzzBuzz")
+    elif number % 3 == 0:
+        print("Fuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
