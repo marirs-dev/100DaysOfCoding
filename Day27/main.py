46e9923b6bbf70c5880124ed6b8b2b4485c9cd77
Sriram48 <ram.k160796@gmail.com>
2021-08-25 05:10:28 +0530

    day 27 completed

---
+++ Day27/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+
+my_label = Label(text="I Am a label", font={"Arial",24,"Bold"})
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text= new_text)
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+
+window.mainloop()
